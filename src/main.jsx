e6c6e42da877f358f7b92e8350f4f34c805d2050
Amigov2 <jade.investissement@gmail.com>
2026-03-11 16:58:06 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+```
+
+**Commit changes** ✅
+
+---
+
+**Fichier 2 — "Add file" → "Create new file"**
+
+Nom :
+```
+index.html
